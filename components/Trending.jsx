0fd9696968113fce9f7f1011e9ca91f480e409be
1DoraGON <moulaykadii2001@gmail.com>
2024-09-25 11:32:23 +0100

    adding search

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -2,6 +2,7 @@ import { View, Text, FlatList, TouchableOpacity, ImageBackground, Image } from '
 import React, { useState } from 'react'
 import * as Animatable from 'react-native-animatable'
 import { icons } from '../constants'
+import { ResizeMode, Video } from 'expo-av'
 
 const  zoomIn = {
   0: {
@@ -24,12 +25,13 @@ const TrendingItem = ({activeItem, item}) => {
   return (
     <Animatable.View className='mr-5' animation={activeItem === item.$id? zoomIn : zoomOut} duration={500}>
       {play? (
-        <Text className="text-white">Playing</Text>
+        <Video source={{uri:item.video}} className="w-52 h-72 rounded-[35px] mt-3 bg-white/10" resizeMode={ResizeMode.CONTAIN} useNativeControls shouldPlay onPlaybackStatusUpdate={(status)=>{ if(status.didJustFinish) {
+          setPlay(false)
+        }}} />
       ): (
         <TouchableOpacity className="relative justify-center items-center" activeOpacity={0.7} onPress={()=> {setPlay(true)}}>
           <ImageBackground source={{uri: item.thumbnail}} className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40" resizeMode='cover' />
           <Image source={icons.play} className='w-12 h*12 absolute' resizeMode='contain' />
-
         </TouchableOpacity>
       )}
     </Animatable.View>
